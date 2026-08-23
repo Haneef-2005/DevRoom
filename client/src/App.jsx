0ddd3e25cd4d2076bf5261f4d117c3525ae0010a
Haneef-2005 <shaikshahanwaz919gmail.com>
2026-08-23 21:34:51 +0530

    Fix Home import case

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Home from "./pages/Home";
+import Home from "./pages/home";
 import Room from "./pages/Room";
 
 function App() {
