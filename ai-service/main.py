68f8c9f27cafda6979c444782ca2214f617c84d5
Haneef-2005 <shaikshahanwaz919gmail.com>
2026-08-23 23:23:57 +0530

    Allow production frontend in AI CORS

--- ai-service/main.py
+++ ai-service/main.py
@@ -18,7 +18,9 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "http://127.0.0.1:5173"
+        "http://127.0.0.1:5173",
+        "https://devroom-client.onrender.com"
+
     ],
     allow_credentials=True,
     allow_methods=["*"],
